99b283006094c0e2e72f79076d8aea81dafcebfa
Malcolm Franklin <mac.mac5@yahoo.com>
2024-07-17 21:02:05 -0600

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='sidenav'>
+                <a href="#about">About</a>
+            </div>
+        </nav>
+    )
+}
